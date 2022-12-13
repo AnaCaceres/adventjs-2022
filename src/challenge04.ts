92f195e7d8430868d8dded97ce66d8399d445ca3
Ana Cáceres <anadelorenzoCL@gmail.com>
2022-12-13 21:22:50 +0100

    Second test of challenge04 added, boxes should fit if they have the correct size

--- src/challenge04.ts
+++ src/challenge04.ts
@@ -1,3 +1,18 @@
-export default function fitsInOneBox(boxes: {l: number, w: number, h: number}[]) {
-  return false
+type Box = {
+  l: number;
+  w: number;
+  h: number;
+};
+ 
+export default function fitsInOneBox(boxes: Box[]) {
+  let doesBoxesFitInOneBox: boolean = true;
+  let boxMeasurementDefinition: (keyof typeof boxes[0])[] = (Object.keys(boxes[0]) as (keyof typeof boxes[0])[]);
+  boxes.reduce((previousBox, box) => {
+    boxMeasurementDefinition.forEach((measurement) => {
+      doesBoxesFitInOneBox = previousBox[measurement] < box[measurement];
+    });
+    return box; 
+  }, boxes[0]);
+
+  return doesBoxesFitInOneBox;
 }
--- tests/challenge04.spec.ts
+++ tests/challenge04.spec.ts
@@ -9,4 +9,13 @@ describe("Challenge #4: Box inside a box and another...", () => {
 
     expect(fitsInOneBox(boxes)).toEqual(expect.any(Boolean));
   });
+
+  it("should return true for boxes with correct size", () => {
+    const boxes = [
+      { l: 1, w: 1, h: 1 },
+      { l: 2, w: 2, h: 2 }
+    ];
+
+    expect(fitsInOneBox(boxes)).toBe(true);
+  });
 });
