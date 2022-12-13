e09b8292f385b331646b39ee658451263ae7c2fa
Ana Cáceres <anadelorenzoCL@gmail.com>
2022-12-13 22:05:47 +0100

    Refactor challenge04

--- src/challenge04.ts
+++ src/challenge04.ts
@@ -3,15 +3,17 @@ type Box = {
   w: number;
   h: number;
 };
- 
+
 export default function fitsInOneBox(boxes: Box[]) {
-  let doesBoxesFitInOneBox: boolean = true;
-  let boxMeasurementDefinition: (keyof typeof boxes[0])[] = (Object.keys(boxes[0]) as (keyof typeof boxes[0])[]);
+  let doesBoxesFitInOneBox = true;
+  let boxMeasurementDefinition = (Object.keys(boxes[0]) as (keyof typeof boxes[0])[]);
   boxes.sort((box1, box2) => (box1.l < box2.l ? -1 : 1));
-  let firstBox: Box = boxes.shift() || boxes[0];
+  let firstBox = boxes.shift() || boxes[0];
   boxes.reduce((previousBox, box) => {
     boxMeasurementDefinition.forEach((measurement) => {
-      if (doesBoxesFitInOneBox) doesBoxesFitInOneBox = previousBox[measurement] < box[measurement];
+      if (doesBoxesFitInOneBox) {
+        doesBoxesFitInOneBox = previousBox[measurement] < box[measurement];
+      }
     });
     return box; 
   }, firstBox);
