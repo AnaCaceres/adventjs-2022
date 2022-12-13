31550e8f9990c489a46c4c4a80815fab9666a501
Ana Cáceres <anadelorenzoCL@gmail.com>
2022-12-13 21:57:57 +0100

    Third and fourth test of challenge04 added

--- tests/challenge04.spec.ts
+++ tests/challenge04.spec.ts
@@ -28,4 +28,24 @@ describe("Challenge #4: Box inside a box and another...", () => {
 
     expect(fitsInOneBox(boxes)).toBe(false);
   });
+
+  it("should return true for boxes with correct size in different order", () => {
+    const boxes = [
+      { l: 1, w: 1, h: 1 },
+      { l: 3, w: 3, h: 3 },
+      { l: 2, w: 2, h: 2 }
+    ]
+
+    expect(fitsInOneBox(boxes)).toBe(true);
+  });
+
+  it("should return false for boxes with incorrect size in different order", () => {
+    const boxes = [
+      { l: 1, w: 1, h: 3 },
+      { l: 1, w: 2, h: 1 },
+      { l: 2, w: 2, h: 2 }
+    ]
+
+    expect(fitsInOneBox(boxes)).toBe(false);
+  });
 });
