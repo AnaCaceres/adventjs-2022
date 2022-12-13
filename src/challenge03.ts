0904d4cec91808d37b3f42d6d8ddcdac33c3d540
Ana Cáceres <anadelorenzoCL@gmail.com>
2022-12-13 20:30:51 +0100

    Añadido segundo test challenge03

--- src/challenge03.ts
+++ src/challenge03.ts
@@ -1,3 +1,18 @@
 export default function distributeGifts(packOfGifts: string[], reindeers: string[]) {
-  return 0;
+  const reindeersLoad: number = calculateMaxReindeersLoad(reindeers);
+  const packOfGiftsLoad: number =calculatePackOfGiftsLoad(packOfGifts);
+
+  return reindeersLoad / packOfGiftsLoad;
+}
+
+function calculateMaxReindeersLoad(reindeers: string[]) {
+  return reindeers.reduce((load: number, reindeer: string) => {
+    return load += 2 * reindeer.length;
+  }, 0);
+}
+
+function calculatePackOfGiftsLoad(packOfGifts: string[]) {
+  return packOfGifts.reduce((load: number, gift: string) => {
+    return load += gift.length;
+  }, 0);
 }
--- tests/challenge03.spec.ts
+++ tests/challenge03.spec.ts
@@ -4,8 +4,16 @@ describe("Challenge #3: How many packs of gifts can Santa carry?", () => {
   it("should return number type", () => {
     const packOfGifts: string[] = ["book", "doll", "ball"];
     const reindeers: string[] = ["dasher", "dancer"];
+    const maximumPacksToDeliver: number = distributeGifts(packOfGifts, reindeers);
+
+    expect(maximumPacksToDeliver).toEqual(expect.any(Number));
+  });
+
+  it("should return 2 maximum packs can be delivered", () => {
+    const packOfGifts: string[] = ["book", "doll", "ball"];
+    const reindeers: string[] = ["dasher", "dancer"];
     const distributedGifts: number = distributeGifts(packOfGifts, reindeers);
 
-    expect(distributedGifts).toEqual(expect.any(Number));
+    expect(distributedGifts).toBe(2);
   });
 });
