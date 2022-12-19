b8a97b112f3cee08b51a7d9552a47c00db45b5a8
Ana Cáceres <anadelorenzoCL@gmail.com>
2022-12-19 18:15:49 +0100

    Challenge06 tests for 1, 2 and 3 dimensions

--- tests/challenge06.spec.ts
+++ tests/challenge06.spec.ts
@@ -6,6 +6,42 @@ describe("Challenge #6: Creating xmas decorations",() => {
 
     const cube = createCube(cubeSize);
 
-    expect(cube).toEqual(expect.any(Number));
+    expect(cube).toEqual(expect.any(String));
+  });
+
+  it("should return one dimension cube for size 1", () => {
+    const cubeSize = 1;
+
+    const cube = createCube(cubeSize);
+
+    expect(cube).toBe('\/\\\_\\\n'+'\\\/\_\/');
+  });
+
+  it("should return two dimension cube for size 2", () => {
+    const cubeSize = 2;
+
+    const cube = createCube(cubeSize);
+
+    expect(cube).toBe(' \/\\\_\\\_\\\n' + '\/\\\/\\\_\\\_\\\n' + '\\\/\\\/\_\/\_\/\n' + ' \\\/\_\/\_\/');
+  });
+
+  it("should return three dimension cube for size 3", () => {
+    const cubeSize = 3;
+
+    const cube = createCube(cubeSize);
+
+    console.log('  \/\\\_\\\_\\\_\\\n' + 
+    ' \/\\\/\\\_\\\_\\\_\\\n' + 
+    '\/\\\/\\\/\\\_\\\_\\\_\\\n' +
+    '\\\/\\\/\\\/\_\/\_\/\_\/\n' +
+    ' \\\/\\\/\_\/\_\/\_\/\n' +
+    '  \\\/\_\/\_\/\_\/')
+
+    expect(cube).toBe('  \/\\\_\\\_\\\_\\\n' + 
+                      ' \/\\\/\\\_\\\_\\\_\\\n' + 
+                      '\/\\\/\\\/\\\_\\\_\\\_\\\n' +
+                      '\\\/\\\/\\\/\_\/\_\/\_\/\n' +
+                      ' \\\/\\\/\_\/\_\/\_\/\n' +
+                      '  \\\/\_\/\_\/\_\/');
   });
 });
