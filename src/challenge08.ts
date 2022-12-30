c2c1b27451653861594f1d03868a1adcf8eef2d2
Ana Cáceres <anadelorenzoCL@gmail.com>
2022-12-30 15:36:20 +0100

    challenge 08 refactor

--- src/challenge08.ts
+++ src/challenge08.ts
@@ -1,3 +1,9 @@
 export default function checkPart(part: string) {
-  return true;
+  let isValidPart = part.toLowerCase() === part.split('').reverse().join('').toLowerCase();
+  let splittedPart = part.split('');
+  splittedPart.forEach( characterToRemove => {
+    let modifiedPart = splittedPart.filter(character => character !== characterToRemove);
+    isValidPart = modifiedPart.join('').toLowerCase() === modifiedPart.reverse().join('').toLowerCase() ? true : isValidPart;
+  });
+  return isValidPart;
 }
--- tests/challenge08.spec.ts
+++ tests/challenge08.spec.ts
@@ -4,4 +4,16 @@ describe("Challenge #8: We need a mechanic!",() => {
   it("should return Boolean type", () => {
     expect(checkPart("")).toEqual(expect.any(Boolean));
   });
+
+  it("should return true for 'uwu'", () => {
+    expect(checkPart("uwu")).toBe(true);
+  });
+
+  it("should return true for 'miidim'", () => {
+    expect(checkPart("miidim")).toBe(true);
+  });
+
+  it("should return false for 'midu'", () => {
+    expect(checkPart("midu")).toBe(false);
+  });
 });
