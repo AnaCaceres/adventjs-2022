1c751f1ba4d2fd672e462563ec122d3647922160
Ana Cáceres <anadelorenzoCL@gmail.com>
2022-12-30 21:52:55 +0100

    Challenge 09 solution

--- src/challenge09.ts
+++ src/challenge09.ts
@@ -1,3 +1,13 @@
 export default function countTime(leds: number[]) {
-  return 0
+  let time = 0;
+
+  while (!leds.every(led => led)) {
+    leds = leds.map((led, index) => {
+      let leftLed = index || leds.length;
+      return +(leds[leftLed - 1] && !led) || led;
+    });
+    time += 7;
+  }
+
+  return time;
 }
--- tests/challenge09.spec.ts
+++ tests/challenge09.spec.ts
@@ -5,4 +5,24 @@ describe("Challenge #9: Crazy Xmas lights",() => {
     const leds = [0, 1, 1, 0, 1];
     expect(countTime(leds)).toEqual(expect.any(Number));
   });
+
+  it("should return 7 seconds", () => {
+    const leds = [0, 1, 1, 0, 1];
+    expect(countTime(leds)).toBe(7);
+  });
+
+  it("should return 21 seconds", () => {
+    const leds = [0, 0, 0, 1];
+    expect(countTime(leds)).toBe(21);
+  });
+
+  it("should return 21 seconds", () => {
+    const leds = [0, 0, 1, 0, 0];
+    expect(countTime(leds)).toBe(28);
+  });
+
+  it("should return 0 seconds", () => {
+    const leds = [1, 1, 1];
+    expect(countTime(leds)).toBe(0);
+  });
 });
