04a8d4f2566beeec8d07ae5ee04b9f9bc50843ab
Ana Cáceres <anadelorenzoCL@gmail.com>
2023-01-03 18:58:00 +0100

    Solution to challenge 10

--- src/challenge10.ts
+++ src/challenge10.ts
@@ -1,3 +1,27 @@
 export default function checkJump(heights: number[]) {
-  return false;
+  let jumpGoesUpDown = true;
+  let maxHeight = Math.max(...heights);
+
+  if ([0, heights.length - 1].includes(heights.indexOf(maxHeight))) {
+    return false;
+  }
+
+  let previousJumps = heights.slice(0, heights.indexOf(maxHeight));
+  let nextJumps = heights.slice(heights.indexOf(maxHeight) + 1, heights.length);
+
+  previousJumps.reduce((previousHeight, height) => {
+    if (previousHeight > height){
+      jumpGoesUpDown = false
+    }
+    return height;
+  }, previousJumps[0])
+
+  nextJumps.reduce((previousHeight, height) => {
+    if (previousHeight < height){
+      jumpGoesUpDown = false
+    }
+    return height;
+  }, nextJumps[0]);
+
+  return jumpGoesUpDown;
 }
--- tests/challenge10.spec.ts
+++ tests/challenge10.spec.ts
@@ -5,4 +5,24 @@ describe("Challenge #10:The Santa Claus sleigh jump",() => {
     const heights = [1, 3, 8, 5, 2]
     expect(checkJump(heights)).toEqual(expect.any(Boolean));
   });
+
+  it("should return true when jump goes up down", () => {
+    const heights = [1, 3, 8, 5, 2]
+    expect(checkJump(heights)).toBe(true);
+  })
+
+  it("should return false when jump goes through same height", () => {
+    const heights = [2, 2, 2, 2]
+    expect(checkJump(heights)).toBe(false);
+  })
+
+  it("should return false when jump goes only up", () => {
+    const heights = [1, 2, 3]
+    expect(checkJump(heights)).toBe(false);
+  })
+
+  it("should return false when jump goes only up", () => {
+    const heights = [1, 7, 3, 5]
+    expect(checkJump(heights)).toBe(false);
+  })
 });
