5acf0be5011b34bcd496e5b3a35788a2274b07bd
Ana Cáceres <anadelorenzoCL@gmail.com>
2023-01-04 13:44:18 +0100

    Challenge 11 simplication of fractions

--- src/challenge11.ts
+++ src/challenge11.ts
@@ -2,8 +2,21 @@ export default function getCompleted(part: string, total: string) {
   let partTime = part.split(":");
   let totalTime = total.split(":")
 
-  let partHours = parseInt(partTime[0]);
-  let totalHours = parseInt(totalTime[0]);
+  let partHours = Number(partTime[0]);
+  let totalHours = Number(totalTime[0]);
+  let partMinutes = Number(partTime[1]);
+  let totalMinutes = Number(totalTime[1]);
+  let partSeconds = Number(partTime[2]);
+  let totalSeconds = Number(totalTime[2]);
+  let result = '1/1';
 
-  return `${partHours}/${totalHours}`;
+  for (var i = Math.max(partHours, totalHours); i > 1; i--) {
+    if ((partHours % i == 0) && (totalHours % i == 0)) {
+      partHours /= i;
+      totalHours /= i;
+    }
+  }
+  result = partHours.toString() + '/' + totalHours.toString();
+
+  return result;
 }
--- tests/challenge11.spec.ts
+++ tests/challenge11.spec.ts
@@ -7,9 +7,21 @@ describe('Challenge #11: Santa Claus is Scrum Master', () => {
     expect(getCompleted(workedTime, totalTaskTime)).toEqual(expect.any(String));
   });
 
-  it('should return string type', () => {
+  it('should return  1/3 for 1h work and 3h task', () => {
     let totalTaskTime = '03:00:00';
     let workedTime = '01:00:00';
     expect(getCompleted(workedTime, totalTaskTime)).toBe('1/3');
+  });
+
+  it('should return 1/2 for 2h work and 4h task', () => {
+    let totalTaskTime = '04:00:00';
+    let workedTime = '02:00:00';
+    expect(getCompleted(workedTime, totalTaskTime)).toBe('1/2');
+  });
+
+  it('should return 1/1 for 1h work and 1h task', () => {
+    let totalTaskTime = '01:00:00';
+    let workedTime = '01:00:00';
+    expect(getCompleted(workedTime, totalTaskTime)).toBe('1/1');
   });
 });
