0d27aae4f3ee2ecdd2182d568728f314ba7579d3
Ana Cáceres <anadelorenzoCL@gmail.com>
2023-01-04 13:32:50 +0100

    Challenge 11 second test

--- src/challenge11.ts
+++ src/challenge11.ts
@@ -1,3 +1,9 @@
 export default function getCompleted(part: string, total: string) {
-  return '1/1';
+  let partTime = part.split(":");
+  let totalTime = total.split(":")
+
+  let partHours = parseInt(partTime[0]);
+  let totalHours = parseInt(totalTime[0]);
+
+  return `${partHours}/${totalHours}`;
 }
--- tests/challenge11.spec.ts
+++ tests/challenge11.spec.ts
@@ -5,5 +5,11 @@ describe('Challenge #11: Santa Claus is Scrum Master', () => {
     let totalTaskTime = '03:00:00';
     let workedTime = '01:00:00';
     expect(getCompleted(workedTime, totalTaskTime)).toEqual(expect.any(String));
-  })
+  });
+
+  it('should return string type', () => {
+    let totalTaskTime = '03:00:00';
+    let workedTime = '01:00:00';
+    expect(getCompleted(workedTime, totalTaskTime)).toBe('1/3');
+  });
 });
