a43318865669bccdf54402fc9768d14bcbde04f2
Ana Cáceres <anadelorenzoCL@gmail.com>
2023-01-04 16:46:40 +0100

    Challenge 11 solution

--- src/challenge11.ts
+++ src/challenge11.ts
@@ -1,22 +1,22 @@
 export default function getCompleted(part: string, total: string) {
-  let partTime = part.split(":");
-  let totalTime = total.split(":")
+  const [partHours, partMinutes, partSeconds] = part.split(":");
+  const [totalHours, totalMinutes, totalSeconds] = total.split(":");
 
-  let partHours = Number(partTime[0]);
-  let totalHours = Number(totalTime[0]);
-  let partMinutes = Number(partTime[1]);
-  let totalMinutes = Number(totalTime[1]);
-  let partSeconds = Number(partTime[2]);
-  let totalSeconds = Number(totalTime[2]);
-  let result = '1/1';
+  let totalHoursInSeconds =
+    +totalSeconds + (+totalMinutes + +totalHours * 60) * 60;
+  let totalPartInSeconds = +partSeconds + (+partMinutes + +partHours * 60) * 60;
 
-  for (var i = Math.max(partHours, totalHours); i > 1; i--) {
-    if ((partHours % i == 0) && (totalHours % i == 0)) {
-      partHours /= i;
-      totalHours /= i;
-    }
-  }
-  result = partHours.toString() + '/' + totalHours.toString();
+  const calculateGreatestCommonDivisor = (a: number, b: number): number => {
+    return b ? calculateGreatestCommonDivisor(b, a % b) : a;
+  };
 
-  return result;
+  const greatestCommonDivisor = calculateGreatestCommonDivisor(
+    totalPartInSeconds,
+    totalHoursInSeconds
+  );
+
+  const numerator = totalPartInSeconds / greatestCommonDivisor;
+  const denominator = totalHoursInSeconds / greatestCommonDivisor;
+
+  return `${numerator}/${denominator}`;
 }
--- tests/challenge11.spec.ts
+++ tests/challenge11.spec.ts
@@ -1,27 +1,33 @@
-import getCompleted from '../src/challenge11';
+import getCompleted from "../src/challenge11";
 
-describe('Challenge #11: Santa Claus is Scrum Master', () => {
-  it('should return string type', () => {
-    let totalTaskTime = '03:00:00';
-    let workedTime = '01:00:00';
+describe("Challenge #11: Santa Claus is Scrum Master", () => {
+  it("should return string type", () => {
+    let totalTaskTime = "03:00:00";
+    let workedTime = "01:00:00";
     expect(getCompleted(workedTime, totalTaskTime)).toEqual(expect.any(String));
   });
 
-  it('should return  1/3 for 1h work and 3h task', () => {
-    let totalTaskTime = '03:00:00';
-    let workedTime = '01:00:00';
-    expect(getCompleted(workedTime, totalTaskTime)).toBe('1/3');
+  it("should return  1/3 for 1h work and 3h task", () => {
+    let totalTaskTime = "03:00:00";
+    let workedTime = "01:00:00";
+    expect(getCompleted(workedTime, totalTaskTime)).toBe("1/3");
   });
 
-  it('should return 1/2 for 2h work and 4h task', () => {
-    let totalTaskTime = '04:00:00';
-    let workedTime = '02:00:00';
-    expect(getCompleted(workedTime, totalTaskTime)).toBe('1/2');
+  it("should return 1/2 for 2h work and 4h task", () => {
+    let totalTaskTime = "04:00:00";
+    let workedTime = "02:00:00";
+    expect(getCompleted(workedTime, totalTaskTime)).toBe("1/2");
   });
 
-  it('should return 1/1 for 1h work and 1h task', () => {
-    let totalTaskTime = '01:00:00';
-    let workedTime = '01:00:00';
-    expect(getCompleted(workedTime, totalTaskTime)).toBe('1/1');
+  it("should return 1/1 for 1h work and 1h task", () => {
+    let totalTaskTime = "01:00:00";
+    let workedTime = "01:00:00";
+    expect(getCompleted(workedTime, totalTaskTime)).toBe("1/1");
+  });
+
+  it("should return 1/6 for 10m work and 1h task", () => {
+    let totalTaskTime = "01:00:00";
+    let workedTime = "00:10:00";
+    expect(getCompleted(workedTime, totalTaskTime)).toBe("1/6");
   });
 });
