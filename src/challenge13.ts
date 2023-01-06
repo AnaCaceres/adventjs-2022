7404975495162c2524f1e0c91f3ed1b074be45d5
Ana Cáceres <anadelorenzoCL@gmail.com>
2023-01-06 18:11:55 +0100

    Challenge 13 solution

--- src/challenge13.ts
+++ src/challenge13.ts
@@ -2,5 +2,8 @@ export default function getFilesToBackup(
   lastBackup: number,
   changes: number[][]
 ) {
-  return [];
+  const changedFiles = changes.filter((change) => change[1] > lastBackup);
+  const changedFilesIds = [...new Set(changedFiles.map((file) => file[0]))];
+
+  return changedFilesIds.sort((a, b) => a - b);
 }
--- tests/challenge13.spec.ts
+++ tests/challenge13.spec.ts
@@ -13,4 +13,17 @@ describe("Challenge #13: Backups for Santa Claus files", () => {
 
     expect(getFilesToBackup(lastBackup, changes)).toBeInstanceOf(Array);
   });
+
+  it("should return 1 and 3 files have been modified", () => {
+    const lastBackup = 1546300800;
+    const changes = [
+      [3, 1546301100],
+      [2, 1546300800],
+      [1, 1546300800],
+      [1, 1546300900],
+      [1, 1546301000],
+    ];
+
+    expect(getFilesToBackup(lastBackup, changes)).toEqual([1, 3]);
+  });
 });
