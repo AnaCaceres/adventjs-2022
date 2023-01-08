63ac1d9e527d0f0ea67b1f61df6b3c01714529e7
Ana Cáceres <anadelorenzoCL@gmail.com>
2023-01-08 20:03:05 +0100

    Challenge 14 solution

--- src/challenge14.ts
+++ src/challenge14.ts
@@ -1,16 +1,16 @@
 export default function getOptimalPath(path: number[][]) {
-  let optimalPath = 0;
-  let lastIndex = 0;
-
-  path.forEach((level) => {
-    if (level.length === 1) {
-      optimalPath += level[lastIndex];
-    } else {
-      const isLeftBest = level[lastIndex] < level[lastIndex + 1];
-      optimalPath += isLeftBest ? level[lastIndex] : level[lastIndex + 1];
-      lastIndex = isLeftBest ? lastIndex : lastIndex + 1;
+  const findBestPathFrom = (level: number, position: number): number => {
+    if (level === path.length - 1) {
+      return path[level][position];
     }
-  });
+
+    const slideLeft = findBestPathFrom(level + 1, position);
+    const slideRight = findBestPathFrom(level + 1, position + 1);
+
+    return path[level][position] + Math.min(slideLeft, slideRight);
+  };
+
+  let optimalPath = findBestPathFrom(0, 0);
 
   return optimalPath;
 }
--- tests/challenge14.spec.ts
+++ tests/challenge14.spec.ts
@@ -12,4 +12,16 @@ describe("Challenge #14: The best path", () => {
 
     expect(getOptimalPath(pyramid)).toBe(2);
   });
+
+  it("should return 5 as best path", () => {
+    const pyramid: number[][] = [[0], [3, 4], [9, 8, 1]];
+
+    expect(getOptimalPath(pyramid)).toBe(5);
+  });
+
+  it("should return 8 as best path", () => {
+    const pyramid: number[][] = [[1], [1, 5], [7, 5, 8], [9, 4, 1, 3]];
+
+    expect(getOptimalPath(pyramid)).toBe(8);
+  });
 });
