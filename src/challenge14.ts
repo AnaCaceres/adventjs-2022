5ccb4ead30ffb705d423b00f8efcfef4a0e07004
Ana Cáceres <anadelorenzoCL@gmail.com>
2023-01-08 19:02:14 +0100

    Challenge 14 second test

--- src/challenge14.ts
+++ src/challenge14.ts
@@ -1,3 +1,16 @@
 export default function getOptimalPath(path: number[][]) {
-  return 0;
+  let optimalPath = 0;
+  let lastIndex = 0;
+
+  path.forEach((level) => {
+    if (level.length === 1) {
+      optimalPath += level[lastIndex];
+    } else {
+      const isLeftBest = level[lastIndex] < level[lastIndex + 1];
+      optimalPath += isLeftBest ? level[lastIndex] : level[lastIndex + 1];
+      lastIndex = isLeftBest ? lastIndex : lastIndex + 1;
+    }
+  });
+
+  return optimalPath;
 }
--- tests/challenge14.spec.ts
+++ tests/challenge14.spec.ts
@@ -6,4 +6,10 @@ describe("Challenge #14: The best path", () => {
 
     expect(getOptimalPath(pyramid)).toEqual(expect.any(Number));
   });
+
+  it("should return 2 as best path", () => {
+    const pyramid: number[][] = [[0], [2, 3]];
+
+    expect(getOptimalPath(pyramid)).toBe(2);
+  });
 });
