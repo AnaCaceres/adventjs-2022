0a060de0dd5b95597d0f170a2cefc3cb7ade170a
Ana Cáceres <anadelorenzoCL@gmail.com>
2023-01-09 19:17:16 +0100

    Challenge 15 refactor

--- src/challenge15.ts
+++ src/challenge15.ts
@@ -1,31 +1,44 @@
 export default function decorateTree(base: string) {
-  const decorationP = ["P P", "B R", "R B"];
-  const decorationB = ["B B", "P R", "R P"];
-  const decorationR = ["R R", "B P", "P B"];
-  let baseDecorations = base.split(" ");
-  let decoratedTree: string[] = new Array(baseDecorations.length).fill("");
-  decoratedTree[0] = base;
+  const nextDecorations = {
+    PP: "P",
+    BR: "P",
+    RB: "P",
+    BB: "B",
+    PR: "B",
+    RP: "B",
+    RR: "R",
+    BP: "R",
+    PB: "R",
+  };
 
-  decoratedTree.forEach((level, levelIndex) => {
-    if (levelIndex !== decoratedTree.length - 1) {
-      const decorations = level.split(" ");
-      let nextLevel: string[] = [];
-      decorations.forEach((decoration, decorationIndex) => {
-        if (decorationIndex !== decorations.length - 1) {
-          let decorationBase =
-            decoration + " " + decorations[decorationIndex + 1];
-          if (decorationP.includes(decorationBase)) {
-            nextLevel.push("P");
-          } else if (decorationB.includes(decorationBase)) {
-            nextLevel.push("B");
-          } else if (decorationR.includes(decorationBase)) {
-            nextLevel.push("R");
-          }
-        }
-      });
-      decoratedTree[levelIndex + 1] = nextLevel.join(" ");
+  const addDecoration = (
+    allDecorations: string[],
+    currentDecoration: string,
+    currentIndex: number,
+    originalDecorations: string[]
+  ) => {
+    const nextDecoration = originalDecorations[currentIndex + 1];
+    if (nextDecoration !== undefined) {
+      const baseDecorations = (currentDecoration +
+        nextDecoration) as keyof typeof nextDecorations;
+      allDecorations.push(nextDecorations[baseDecorations]);
     }
-  });
 
-  return decoratedTree.reverse();
+    return allDecorations;
+  };
+
+  const createTree = (
+    tree: string[],
+    _currentDecorations: string,
+    currentLevel: number
+  ) => {
+    const nextLevel = tree[currentLevel].split(" ").reduce(addDecoration, []);
+    tree.push(nextLevel.join(" "));
+
+    return tree;
+  };
+
+  return [...Array(base.split(" ").length - 1)]
+    .reduce(createTree, [base])
+    .reverse();
 }
