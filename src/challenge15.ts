a879abfda59a9f8bad121475b59675cbec5b8d9d
Ana Cáceres <anadelorenzoCL@gmail.com>
2023-01-09 18:27:06 +0100

    Challenge 15 solution

--- src/challenge15.ts
+++ src/challenge15.ts
@@ -1,3 +1,31 @@
 export default function decorateTree(base: string) {
-  return [];
+  const decorationP = ["P P", "B R", "R B"];
+  const decorationB = ["B B", "P R", "R P"];
+  const decorationR = ["R R", "B P", "P B"];
+  let baseDecorations = base.split(" ");
+  let decoratedTree: string[] = new Array(baseDecorations.length).fill("");
+  decoratedTree[0] = base;
+
+  decoratedTree.forEach((level, levelIndex) => {
+    if (levelIndex !== decoratedTree.length - 1) {
+      const decorations = level.split(" ");
+      let nextLevel: string[] = [];
+      decorations.forEach((decoration, decorationIndex) => {
+        if (decorationIndex !== decorations.length - 1) {
+          let decorationBase =
+            decoration + " " + decorations[decorationIndex + 1];
+          if (decorationP.includes(decorationBase)) {
+            nextLevel.push("P");
+          } else if (decorationB.includes(decorationBase)) {
+            nextLevel.push("B");
+          } else if (decorationR.includes(decorationBase)) {
+            nextLevel.push("R");
+          }
+        }
+      });
+      decoratedTree[levelIndex + 1] = nextLevel.join(" ");
+    }
+  });
+
+  return decoratedTree.reverse();
 }
--- tests/challenge15.spec.ts
+++ tests/challenge15.spec.ts
@@ -5,4 +5,14 @@ describe("Challenge #15: Decorating the Christmas tree", () => {
     let base = "B P R P";
     expect(decorateTree(base)).toBeInstanceOf(Array);
   });
+
+  it("should R at the top", () => {
+    let base = "B P R P";
+    expect(decorateTree(base)).toEqual(["R", "P B", "R B B", "B P R P"]);
+  });
+
+  it("should return B at the top", () => {
+    let base = "B B";
+    expect(decorateTree(base)).toEqual(["B", "B B"]);
+  });
 });
