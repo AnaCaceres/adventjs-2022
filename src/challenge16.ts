b2c99af29449cefcb6e0a3417f05a5e3adc6447a
Ana Cáceres <anadelorenzoCL@gmail.com>
2023-01-12 22:19:27 +0100

    Challenge 16 regex refactor

--- src/challenge16.ts
+++ src/challenge16.ts
@@ -1,10 +1,10 @@
 export default function fixLetter(letter: string) {
   return letter
     .trim()
-    .replace(/[.?!] [a-z]|^[a-z]/g, (match) => match.toUpperCase())
     .replace(/([.,?!\s])(?=\1)/g, "")
     .replace(/([.,!?])([a-z])/gi, "$1 $2")
     .replace(/(\s)([.,!?])/g, "$2")
+    .replace(/[.?!]\s[a-z]|^[a-z]/g, (match) => match.toUpperCase())
     .replace(/santa claus/gi, "Santa Claus")
     .replace(/([a-z])$/, "$1.");
 }
