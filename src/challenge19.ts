7496f9786543e56e1652c784905170d26e3e7fbf
Ana Cáceres <anadelorenzoCL@gmail.com>
2023-01-23 18:31:01 +0100

    Challenge 19 solution

--- src/challenge19.ts
+++ src/challenge19.ts
@@ -1,3 +1,8 @@
 export default function sortToys(toys: string[], positions: number[]) {
-  return [];
+  const minPosition = Math.min(...positions);
+  positions = positions.map((position) => position - minPosition);
+  const sortedToys: string[] = Array(positions.length).fill("");
+  positions.forEach((position, index) => (sortedToys[position] = toys[index]));
+
+  return sortedToys;
 }
--- tests/challenge19.spec.ts
+++ tests/challenge19.spec.ts
@@ -6,4 +6,27 @@ describe("Challenge #19: Sorting the toys!", () => {
     const positions = [2, 3, 1, 0];
     expect(sortToys(toys, positions)).toBeInstanceOf(Array);
   });
+
+  it("should return sorted toys starting from 0", () => {
+    const toys = ["ball", "doll", "car", "puzzle"];
+    const positions = [2, 3, 1, 0];
+    expect(sortToys(toys, positions)).toEqual([
+      "puzzle",
+      "car",
+      "ball",
+      "doll",
+    ]);
+  });
+
+  it("should return sorted toys starting from 5", () => {
+    const toys = ["pc", "xbox", "ps4", "switch", "nintendo"];
+    const positions = [8, 6, 5, 7, 9];
+    expect(sortToys(toys, positions)).toEqual([
+      "ps4",
+      "xbox",
+      "switch",
+      "pc",
+      "nintendo",
+    ]);
+  });
 });
